cab91d17937c1b8945af080e0d472db3d9bb62db
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2018-02-03 03:02:32 +0300

    nginx container and gunicorn service added

---
+++ src/gunicorn-config.py
@@ -0,0 +1,12 @@
+"""gunicorn WSGI server configuration."""
+from multiprocessing import cpu_count
+
+def max_workers():
+    return cpu_count()
+
+# bind = 'unix:src.sock'
+bind = '0.0.0.0:8000'
+max_requests = 1000
+workers = max_workers()
+errorlog = 'logs/gun-error.log'
+accesslog = 'logs/gun-access.log'
