c415573400dc31541165d78ebddb9b8c1ed728d5
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2018-02-05 00:03:17 +0300

    node dev server as separate container

--- src/src/settings.py
+++ src/src/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': constants.FRAMEWORK_DB,
-#         'USER': constants.FRAMEWORK_DB_USER,
-#         'PASSWORD': constants.FRAMEWORK_DB_PASS,
-#         'HOST': constants.FRAMEWORK_DB_HOST,
-#         'PORT': constants.FRAMEWORK_DB_PORT
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': constants.FRAMEWORK_DB,
+        'USER': constants.FRAMEWORK_DB_USER,
+        'PASSWORD': constants.FRAMEWORK_DB_PASS,
+        'HOST': constants.FRAMEWORK_DB_HOST,
+        'PORT': constants.FRAMEWORK_DB_PORT
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- src/web/fetcher_app/containers/InitialContainer.jsx
+++ src/web/fetcher_app/containers/InitialContainer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default class InitialContainer extends React.Component {
     render() {
         return (
-            <div>Hello world 9!</div>
+            <div>Hello world 10!</div>
         )
     }
 }
